515af15b62f88daec74c7d93d1c29b62dd0b8e11
Brendan Mulholland <brendan@recitalapp.com>
2024-08-28 11:19:02 +0200

    Fix filtering now, I hope

--- script.sh
+++ script.sh
@@ -40,12 +40,9 @@ if [ "$INPUT_REPORTER" = "github-pr-review" ]; then
 else
 
   # shellcheck disable=SC2086
-  "$(npm root)"/.bin/prettier --check ${INPUT_PRETTIER_FLAGS} 2>&1 | sed --regexp-extended 's/(\[warn\].*)$/\1 1 File is not properly formatted./' |
+  "$(npm root)"/.bin/prettier --check "${INPUT_PRETTIER_FLAGS}" 2>&1 | sed --regexp-extended 's/(\[warn\].*)$/\1 File is not properly formatted./' |
     reviewdog \
-      -efm="%-G[warn] Code style issues found in the above file%.%+" \
-      -efm="%-G[warn] Code style issues found in %\d%+ files. %.%+" \
-      -efm="[%tarn] %f" \
-      -efm="[%tarn] %f %l %m" \
+      -efm="[%tarn] %f %m" \
       -efm="%E[%trror] %f: %m (%l:%c)" \
       -efm="%C[error]%r" \
       -efm="%Z[error]%r" \
